35354441b1119db488057b9609cd3c0c72b00c2f
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-12-10 23:25:27 +0300

    added file with global window variables

--- src/main/java/com/racing/client/Main.java
+++ src/main/java/com/racing/client/Main.java
@@ -3,13 +3,11 @@ package com.racing.client;
 import javax.swing.*;
 
 public class Main {
-    static final int W_WIDTH = 840;
-    static final int W_HEIGHT = 650;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Socket racer");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(W_WIDTH, W_HEIGHT);
+        frame.setSize(Settings.W_WIDTH, Settings.W_HEIGHT);
 
         frame.add(new Road());
         frame.setVisible(true);
---
+++ src/main/java/com/racing/client/Settings.java
@@ -0,0 +1,7 @@
+package com.racing.client;
+
+public class Settings {
+    static final int W_WIDTH = 840;
+    static final int W_HEIGHT = 685;
+    static final int HEADER_HEIGHT = 35;
+}
